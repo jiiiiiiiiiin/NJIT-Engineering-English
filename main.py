9e3ece7de1f0e863d5f4f64dcca66366b202955f
jiiiiiiiiiin <30288323+jiiiiiiiiiin@users.noreply.github.com>
2020-12-05 23:39:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ browser.set_window_size(1200, 900)
 print("开始工作...\n")
 
 # 从这底下开始填 刷完的在每行前面加一个 “#”号 就可以注释掉
-English(username="202190236", password="123456", browser=browser, unit=[1,2,3], shot=True)
+English(username="", password="123456", browser=browser, unit=[1,2,3], shot=True)
 
 
 browser.close()
